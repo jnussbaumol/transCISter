4f4ba42329693c65e7e1425788c77943545906c7
Joshua Nussbaum <145164917+jnussbaumol@users.noreply.github.com>
2025-04-02 13:34:46 -0500

    start refactor, now with AI

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+from pathlib import Path
 from pypdf import PdfReader
 from tiktoken import encoding_for_model
 from openai import OpenAI
+from dotenv import load_dotenv
 
-TOKEN_BUDGET: int = 4096
+TOKEN_BUDGET: int = 128_000
 SYSTEM_PROMPT: str = """
 You are a helpful AI assistant that transforms text that has been OCR'd from a PDF and pull out information on the requirements for a security configuration benchmark.
-The user's messages will comprise of the text that is to be analyzed. You are to divide the Recommendations into two categories:
-
-- Level 1
-- Level 2
-
-Respond to the user with a list of every Level 1 recommendation, and a second list of every Level 2 recommendation.
 """
-MODEL = "gemma-3-27b-it"
+# MODEL = "gemma-3-27b-it"
+MODEL = "gpt-4o"
 TEMPERATURE = 0.7
 
 
@@ -24,7 +21,8 @@ def budget_count(token_count: int) -> int:
     return TOKEN_BUDGET - token_count
 
 
-def main(pdf: str, local: bool = True) -> None:
+def main(pdf: str, local: bool = False) -> None:
+    load_dotenv()
     reader: PdfReader = PdfReader(pdf)
     result: str = ""
 
@@ -32,17 +30,16 @@ def main(pdf: str, local: bool = True) -> None:
     total_tokens = 0
 
     for page in reader.pages:
-        running_tokens = total_tokens
         contents: str = page.extract_text()
         result += contents
         token_count = len(encoding.encode(contents))
         total_tokens += token_count
         if over_budget(total_tokens):
             print(f"over budget by {budget_count(total_tokens)}")
-    # with open("output.txt", "w") as file:
-    #     file.write(result)
     print(total_tokens)
 
+    with open("output.txt", "w") as file:
+        file.write(result)
     # TODO: extract to factory for external vs lmstudio vs ollama
     if local:
         client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
@@ -54,12 +51,15 @@ def main(pdf: str, local: bool = True) -> None:
             messages=[
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": result},
+                {"role": "user", "content": "What are all the Level 1s?"},
             ],
             temperature=TEMPERATURE,
         )
-        print(completions.choices[0].message)
+        print(completions.choices[0].message.content)
     else:
-        print("too big")
+        print("too big, parse it instead")
+        with open("output.txt", "w") as file:
+            file.write(result)
 
 
 if __name__ == "__main__":
